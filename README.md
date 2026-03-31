89ef1687a1e04dc118205ee658c3631b7a5318ac
JindrichDvorak <jindrich.dvorak.development@gmail.com>
2026-03-31 14:49:25 +0200

    Added partial documentation

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # SQLiteCs-Project
+
+> Řešení praktických úloh v `C#`, které vyžadují interakci s `SQLite` databázemi.
+
+## Základní architektura
+
+Tento repozitář obsahuje `Visual Studio` řešení `SQLiteCs-Project`, které dále obsahuje další dva projekty:
+
+- `SQLiteCs` - Statická knihovna, která definuje jednoduchou abstrakci složitějších procesů zajišťujících interakci s `SQLite` databázemi pomocí `C#` příkazů. Tato knihovna má dvě hlavní závislosti (které se **automaticky nainstalují** po spuštění první kompilace pomocí package manageru `NuGet`):
+  - `System.Data.SQLite` - Propojení `SQLite` databáze a `C#`.
+  - `SQLitePCLRaw.bundle_e_sqlite3` - Implementace `SQLite` databáze.
+- `ProjectApp` - Konzolová aplikace, která se odkazuje na statickou knihovnu `SQLiteCs` a obsahuje následující dva soubory:
+  - `Program.cs` - Obsahuje `Main` metodu, jedná se tedy o vstupní bod celé aplikace. 
+  - `csfd.db` - Příkladová databáze obsahující jedinou tabulku `movies`, ve které jsou uloženy záznamy o 100 nejlépe hodnocených filmech podle ČSFD. Tato tabulka obsahuje následující atributy:
+    - `id` - ID záznamu.
+    - `title` - Název filmu.
+    - `year` - Rok vydání filmu.
+    - `country` - Země původu.
+    - `genre` - Žánr / žánry.
+    - `director` - Režisér / režiséři.
+    - `actors` - První dva uvedení herci.
+    - `rating` - Hodnocení.
+    - `num_of_ratings` - Počet hodnocení.
+
+## Základní použití
+
+Pro úspěšné řešení všech připravených úloh není potřeba nijak zasahovat do zdrojového kódu knihovny `SQLiteCs`. Veškerá logika naší aplikace bude obsažena ve zdrojových souborech konzolové aplikace `ProjectApp`.
+
+Při práci s databázemi obecně sledujeme následující kroky:
+
+- Otevření databáze.
+- Interakce s databází.
+- Uzavření databáze.
+
+### Otevření databáze
+
+Knihovna `SQLiteCs` pracuje s databázemi, které jsou uloženy lokálně na disku. Proto je potřeba specifikovat takzvanou cestu (`path`) k umístění databáze, aby mohlo vzniknout spojení mezi danou databází a naším `C#` programem. Například, pro otevření databáze `csfd.db` použijeme příkaz:
+```cs
+Database db = new Database("csfd.db");
+```
+Skrze nově vytvořený objekt `db` je následně možné s databází `csfd.db` interagovat (čemuž se věnuje následující sekce). 
+
+Pokud bychom chtěli vytvořit kompletně novou databázi, použijeme stejný příkaz, ale specifikujeme jinou cestu, která zároveň udává i název souboru, který bude novou databázi obsahovat (v tomto případě `newDatabase.db`):
+```cs
+Database db = new Database("newDatabase.db");
+```
+Tento příkaz automaticky vytvoří nový soubor s názvem `newDatabase.db` ve stejné složce, ze které se spouští náš program -- používali jsme tedy takzvané **relativní cesty**.
+
+Knihovna `SQLiteCs` umožňuje otevřít databázi i v "libovolném" adresáři. Za tímto účelem bychom použili takzvanou **absolutní cestu**. Například, pokud se databáze `csfd.db` nachází přímo na disku `C`, otevřeme ji následujícím příkazem (protože symbolem `\` začínají takzvané escape sekvence, musíme místo `\` psát `\\`):
+```cs
+Database db = new Database("C:\\csfd.db");
+```
+Tímto způsobem je možné otevřít (nebo vytvořit) "libovolné" množství **různých** databází. 
+
+`SQL` příkazy modifikují trvalou paměť (přepisují data v souboru na disku), a proto při jejich používání vzniká riziko, že nějaký jejich nezamýšlený vedlejší efekt nenávratně přepíše databázi, což 
--- SQLiteCs/Database.cs
+++ SQLiteCs/Database.cs
@@ -6,10 +6,25 @@ namespace SQLiteCs
 {
     public struct QueryResult(string[] columnNames, Type[] columnDataTypes, object?[][] rows, int rowCount)
     {
+        /// <summary>
+        /// Pole (<c>string</c>) obsahující názvy vrácených atributů (sloupců).
+        /// </summary>
         public string[] ColumnNames = columnNames;
+        /// <summary>
+        /// Pole (<c>Type</c>) obsahující datové typy vrácených atributů (sloupců).
+        /// </summary>
         public Type[] ColumnDataTypes = columnDataTypes;
+        /// <summary>
+        /// Pole (<c>object?[]</c>) obsahující všechny vrácené záznamy (řádky) jako pole (<c>object?</c>).
+        /// </summary>
         public object?[][] Rows = rows;
+        /// <summary>
+        /// Počet vrácených atributů (sloupců).
+        /// </summary>
         public int ColumnCount = columnNames.Length;
+        /// <summary>
+        /// Počet vrácených záznamů (řádků).
+        /// </summary>
         public int RowCount = rowCount;
     }
 
@@ -21,6 +36,13 @@ namespace SQLiteCs
         private bool _testingMode = false;
         private bool _sqlExceptions = false;
 
+        /// <summary>
+        /// Konstruktor třídy <c>Database</c>, která umožňuje interagovat s SQLite databází uvnitř C# aplikace. 
+        /// Vstupním parametrem je <c>databasePath</c>, což je <c>string</c> reprezentující cestu k souboru databáze 
+        /// (relativní vůči .exe souboru C# aplikace).
+        /// </summary>
+        /// <param name="databasePath">Relativní cesta k souboru databáze (vůči .exe souboru C# aplikace). Pokud daný soubor neexistuje, automaticky 
+        /// se vytvoří nový.</param>
         public Database(string databasePath)
         {
             _databasePath = databasePath;
@@ -45,6 +67,10 @@ namespace SQLiteCs
             _connection.Dispose();
         }
 
+        /// <summary>
+        /// Metoda <c>CloseDatabase()</c> uzavírá spojení mezi C# programem a databází. Pokud je databáze právě v testovacím módu (který se zapíná 
+        /// příkazem: <c>SetTestingMode(true);</c>), tak metoda <c>CloseDatabase()</c> po uzavření spojení i vymaže její obsah.
+        /// </summary>
         public void CloseDatabase()
         {
             if (_connection != null)
@@ -97,6 +123,12 @@ namespace SQLiteCs
             return new QueryResult(columnNames, columnDataTypes, rows, rowCount);
         }
 
+        /// <summary>
+        /// Funkce <c>Query()</c> umožňuje C# programu pokládat dotazy připojené databázi pomocí SQL dotazů, které přijímá jako vstupní 
+        /// <c>string</c> parametr a následně vrací výsledek dotazu jako objekt typu <c>QueryResult</c>. 
+        /// </summary>
+        /// <param name="sql">SQL dotaz.</param>
+        /// <returns>Všechny výsledky položeného SQL dotazu uložené v objektu typu <c>QueryResult</c>.</returns>
         public QueryResult Query(string sql)
         {
             DataTable dataTable = new DataTable();
@@ -117,6 +149,13 @@ namespace SQLiteCs
             return CreateQueryResult(dataTable);
         }
 
+        /// <summary>
+        /// Funkce <c>Scalar()</c> umožňuje C# programu pokládat dotazy připojené databázi pomocí SQL dotazů, které přijímá jako vstupní 
+        /// <c>string</c> parametr a následně vrací výsledek dotazu jako obecný objekt typu <c>object?</c>. Pokud má SQL dotaz vrátit 
+        /// tabulku, tak tato funkc vrátí pouze prvek dopovídající prvnímu řádku prvního sloupce této tabulky.
+        /// </summary>
+        /// <param name="sql">SQL dotaz.</param>
+        /// <returns>Jediný výsledek položeného SQL dotazu jako <c>object?</c>.</returns>
         public object? Scalar(string sql)
         {
             try
@@ -134,6 +173,12 @@ namespace SQLiteCs
             }
         }
 
+        /// <summary>
+        /// Funkce <c>NonQuery()</c> umožňuje C# programu provádět na databázi SQL příkazy (modifikace databáze), které přijímá jako 
+        /// vstupní <c>string</c> parametr a následně vrací informaci (jako pravdivostní hodnotu) o tom, zdali byl daný příkaz úspěšný.
+        /// </summary>
+        /// <param name="sql">SQL příkaz.</param>
+        /// <returns>Informaci o úspěchu provedení zadaného SQL příkazu.</returns>
         public bool NonQuery(string sql)
         {
             try
@@ -152,6 +197,15 @@ namespace SQLiteCs
             }
         }
 
+        /// <summary>
+        /// Funkce <c>CopyDatabase()</c> umožňuje vytvořit kopii již existující databáze pod jiným jménem. Funkce přijímá dva vstupní parametry, 
+        /// které reprezentují cestu k souboru databáze, kterou chceme zkopírovat a cestu k novému (nebo již exisutjícímu) souboru, který má 
+        /// obsahovat kopii původní databáze. Obdobně jako při vytváření nové databáze, pokud zadaná cesta k nové databázi ukazuje na prozatím neexistující soubor, 
+        /// tato funkce tento soubor automaticky vytvoří (stejné chování ovšem neplatí pro cestu k databázi, kterou chceme zkopírovat).
+        /// </summary>
+        /// <param name="databasePath">Cesta k souboru obsahujícím databázi, kterou chceme zkopírovat (tento soubor musí existovat).</param>
+        /// <param name="databaseCopyPath">Cesta k souboru nové databáze, která má být kopií původní databáze (nemusí před vytvořením kopie existovat).</param>
+        /// <returns>Referenci na nově vzniklou databázi.</returns>
         public static Database? CopyDatabase(string databasePath, string databaseCopyPath)
         {
             try
@@ -168,6 +222,13 @@ namespace SQLiteCs
             }
         }
 
+        /// <summary>
+        /// Funkce <c>CopyDatabase()</c> umožňuje vytvořit kopii této databáze pod jiným jménem. Funkce přijímá vstupní parametr reprezentující cestu k nové databázi, 
+        /// která má být kopií této databáze. Obdobně jako při vytváření nové databáze, pokud zadaná cesta k nové databázi ukazuje na prozatím neexistující soubor, 
+        /// tato funkce tento soubor automaticky vytvoří.
+        /// </summary>
+        /// <param name="databaseCopyPath">Cesta k souboru nové databáze, která má být kopií této databáze (nemusí před vytvořením kopie existovat).</param>
+        /// <returns>Referenci na nově vzniklou databázi.</returns>
         public Database? CopyDatabase(string databaseCopyPath)
         {
             try
@@ -184,6 +245,10 @@ namespace SQLiteCs
             }
         }
 
+        /// <summary>
+        /// Funkce <c>ClearDatabase()</c> vymaže obsah této databáze, ale zachová otevřené spojení mezi C# programem a nově promazanou databází.
+        /// </summary>
+        /// <returns>Informaci o tom, zdali bylo promazání obsahu databáze úspěšné.</returns>
         public bool ClearDatabase()
         {
             if (!_testingMode)
@@ -209,11 +274,22 @@ namespace SQLiteCs
             }
         }
 
+        /// <summary>
+        /// Metoda <c>SetTestingMode()</c> umožňuje nastavit testovací mód této databáze. Pokud je testovací mód zapnutý (<c>true</c>), pak je možné vymazat obsah databáze pomocí funkce 
+        /// <c>ClearDatabase()</c>, nebo metody <c>CloseDatabase()</c>, kterou bychom měli volat pro každou databázi na konci našeho programu.
+        /// </summary>
+        /// <param name="enable">Pravdivostní hodnota určující, zdali má být testovací mód pro tuto databázi zapnutý, nebo vypnutý.</param>
         public void SetTestingMode(bool enable)
         { 
             _testingMode = enable;
         }
 
+        /// <summary>
+        /// Metoda <c>SetSQLExceptions()</c> umožňuje změnit reakci programu na chyby v SQL dotazech a příkazech. Pokud jako vstupní parametr této funkce zadáme hodnotu <c>true</c>, 
+        /// případné SQL chyby zastaví chod programu vyvoláním výjimky. Defaultní reakcí na SQL chyby je vypsání chybové zprávy do konzole bez zastavení chodu programu.
+        /// </summary>
+        /// <param name="enable">Pravdivostní hodnota určující, zdali mají chyby v SQL dotazech a příkazech vyvolávat výjimky, nebo jestli má program dále pokračovat a 
+        /// chyby vypsat do konzole.</param>
         public void SetSQLExceptions(bool enable)
         { 
             _sqlExceptions = enable;
